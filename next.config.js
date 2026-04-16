82a50fa19aadfbc8853a32d6fd0a910e722dfff1
mayurantala-coder <hyperlinkmayura123@gmail.com>
2026-04-16 17:14:45 +0530

    Add next.config.js for Next.js project configuration
    web design

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Next-js-project1',
+  assetPrefix: '/Next-js-project1/',
+};
+
+module.exports = nextConfig;
