4e1e4d5e12933394e31be05840f62262e7535855
Mayur Antala <134778638+MayurAntala@users.noreply.github.com>
2026-04-16 17:40:42 +0530

    update

---
+++ out/_next/static/C7REh6HR1-OVcKJQc9TWq/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/webdesign/Next-js-project1//_next/:path+",
+        "destination": "/webdesign/Next-js-project1/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
